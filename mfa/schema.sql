4007538791f264aba50829c357a8d2451d83c528
Rabby-khan-04 <ajrabbyk72@gmail.com>
2026-08-08 18:35:32 +0600

    Updated the mfa scheam

--- mfa/schema.sql
+++ mfa/schema.sql
@@ -1,3 +1,5 @@
+PRAGMA foreign_keys = ON;
+
 CREATE TABLE "collections" (
     "id" INTEGER,
     "title" TEXT NOT NULL,
@@ -5,3 +7,17 @@ CREATE TABLE "collections" (
     "acquired" NUMERIC,
     PRIMARY KEY("id")
 );
+
+CREATE TABLE "artists" (
+    "id" INTEGER,
+    'name' TEXT NOT NULL,
+    PRIMARY KEY("id")
+);
+
+CREATE TABLE "created" (
+    "artist_id" INTEGER,
+    "collection_id" INTEGER,
+    PRIMARY KEY("artist_id", "collection_id"),
+    FOREIGN KEY("artist_id") REFERENCES "artists"("id") ON DELETE CASCADE,
+    FOREIGN KEY("collection_id") REFERENCES "collections"("id") ON DELETE CASCADE
+)
